2a2ebf65484e2b254a5f8e630486d91d47e7b619
DrinKurtishi <kurtishidrin@gmail.com>
2022-12-29 10:38:44 +0100

    incorporated random computer input

--- rps.js
+++ rps.js
@@ -1,1 +1,12 @@
-console.log("Hello niggga")
+let UserInput;
+let Computerchoice;
+let array = ['r', 'p', 's'];
+console.log("Lets play Rock Paper Scissors!")
+console.log("Press 'r' for rock")
+console.log("Press 'p' for paper")
+console.log("Press 's' for scissors")
+UserInput = prompt("Enter your choice here:")
+Computerchoice = Math.floor(Math.random() * array.length);
+console.log("Computers choice is")
+console.log(Computerchoice, array[Computerchoice]);
+
