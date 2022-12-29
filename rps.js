68882155b49a932f189b4f758c565c9e7db62748
DrinKurtishi <kurtishidrin@gmail.com>
2022-12-29 14:09:59 +0100

    started finishing game

--- rps.js
+++ rps.js
@@ -1,12 +1,36 @@
+//start with game 
+function PlayRound(UserInput, Computerchoice){
+    let status;
+    if(UserInput === Computerchoice)
+    {
+            status = "Draw!";
+    }
+    return status;
+
+}
 let UserInput;
 let Computerchoice;
-let array = ['r', 'p', 's'];
-console.log("Lets play Rock Paper Scissors!")
-console.log("Press 'r' for rock")
-console.log("Press 'p' for paper")
-console.log("Press 's' for scissors")
-UserInput = prompt("Enter your choice here:")
-Computerchoice = Math.floor(Math.random() * array.length);
-console.log("Computers choice is")
-console.log(Computerchoice, array[Computerchoice]);
+let array = ['rock', 'paper', 'scissors'];
+console.log("Lets play Rock Paper Scissors!");
+UserInput = prompt("Enter your choice here:");
+UserInput = UserInput.toLowerCase();//turn lowercase
+console.log("You typed:");
+console.log(UserInput);
+
+if(UserInput === 'rock'){//change value based on array postiton for comparison
+    UserInput = 0;
+}
+else if(UserInput === 'paper'){
+    UserInput = 1;
+}
+else if(UserInput === 'scissrors'){
+    UserInput = 2;
+}
+
+Computerchoice = Math.floor(Math.random() * array.length);//select random element from array
+console.log("Computers choice is");
+console.log(array[Computerchoice]);
+
+console.log(PlayRound(UserInput, Computerchoice));
+
 
