6636bf11d7f78de471adaec274d64bcfccab9982
DrinKurtishi <kurtishidrin@gmail.com>
2023-01-06 11:00:13 +0100

    Added UI and completely reworked the js file

--- rps.js
+++ rps.js
@@ -1,90 +1,79 @@
-UCount = 0;//keep track of score
-Pcount = 0;
-//start with game 
-//rock is 0, paper is 1, scissors is 2
-function PlayRound(UserInput, Computerchoice){
-    let status;
+let rockbtn = document.querySelector('#rock');
+let paperbtn = document.querySelector('#paper');
+let scissorsbtn = document.querySelector('#scissors');
+let array = ['rock', 'paper', 'scissors'];
+let UserInput;
+let Computerchoice;
+let UCount = 0;//user score
+let Pcount = 0;//Computer score
+
+function compare(UserInput, Computerchoice)
+{
     //if theyre the same do this
     if(UserInput === Computerchoice){
-        status = "Draw!";
+        document.getElementById("output").innerHTML = "Draw!"
     }
     //when user inputs rock
     else if(UserInput === 0 && Computerchoice === 1){
-        status = "You lose! Paper beats rock."
         Pcount++;//update score
+        document.getElementById("output").innerHTML = "You lose! Paper beats rock."
     }
     else if(UserInput === 0 && Computerchoice === 2){
-        status = "You win! Rock beats scissors."
         UCount++;
+        document.getElementById("output").innerHTML = "You win! Rock beats scissors."
     }
     //when user inputs paper
     else if(UserInput === 1 && Computerchoice === 0){
-        status = "You win! Paper beats rock"
         UCount++;
+        document.getElementById("output").innerHTML = "You win! Paper beats rock"
     }
     else if(UserInput === 1 && Computerchoice === 2){
-        status = "You lose! Scissors beat paper."
         Pcount++;
+        document.getElementById("output").innerHTML = "You lose! Scissors beat paper."
     }
     //when user inputs scissors
     else if(UserInput === 2 && Computerchoice === 0){
-        status = "You lose! Rock beats scissors."
         Pcount++;
+        document.getElementById("output").innerHTML = "You lose! Rock beats scissors."
     }
     else if(UserInput === 2 && Computerchoice === 1){
-        status = "You win! Scissors beats paper."
-        UCount++
+        UCount++;
+        document.getElementById("output").innerHTML = "You win! Scissors beats paper."
     }
-    return status;
-}
-let UserInput;
-let Computerchoice;
-let array = ['rock', 'paper', 'scissors'];
+    document.getElementById('scoreboard').innerHTML = "<br> Your score: " + UCount + "<br>Computers score: " + Pcount;
+    //determine winner
+    if(UCount === 5 || Pcount === 5)
+    {
+       //disable buttons after either player reaches 5 wins
+       document.querySelector("#rock").disabled = true;
+       document.querySelector("#paper").disabled = true;
+       document.querySelector("#scissors").disabled = true;
+       //determine winnter
+       if(UCount === 5)
+       {
+            document.getElementById("output").innerHTML = "You defeated the computer!"; 
+       }
+       else
+       {
+            document.getElementById("output").innerHTML = "You have been defeated by the computer!";
+       }
 
-console.log("Lets play Rock Paper Scissors! First to 5 wins.");
-let i = 0;
-let round = 1;
-while(UCount < 5 && Pcount < 5)
-{
-    //print round
-    console.log("Round", round++);
-   
-    UserInput = prompt("Enter your choice here:");
-    UserInput = UserInput.toLowerCase();//turn lowercase.
-
-    if(UserInput === 'rock' || UserInput === 'paper' || UserInput === 'scissors'){
-
-        console.log("You chose:", UserInput);
-
-        if(UserInput === 'rock'){//change value based on array postiton for comparison
-            UserInput = 0;
-        }
-        else if(UserInput === 'paper'){
-            UserInput = 1;
-        }
-        else if(UserInput === 'scissors'){
-            UserInput = 2;
-        }
-
-        Computerchoice = Math.floor(Math.random() * array.length);//select random element from array
-        console.log("Computer chose:", array[Computerchoice]);
-        console.log(PlayRound(UserInput, Computerchoice));//use game mechanic
-        //count score
-        console.log("Your score:", UCount); 
-        console.log("Computer's score:", Pcount);
-        console.log("\n");
-    }
-    else{
-        console.log("Invalid input, please refresh page to start over.");
-        break;
     }
 }
-//determine winner only if all rounds were played (user didn't put invalid text)
-if(UCount === 5 || Pcount === 5){
-    if(UCount > Pcount){
-        console.log("You win, but your opponent can't even think, how can you be proud of this achievement? Shame...");
-    }
-    else{
-        console.log("You lose and your oponent can't even think, well done...");
-    }
-}
+
+rockbtn.addEventListener('click', function(){
+    UserInput = 0;//rock
+    Computerchoice = Math.floor(Math.random() * array.length);//random
+    compare(UserInput, Computerchoice);
+});
+paperbtn.addEventListener('click', function(){
+    UserInput = 1;//paper
+    Computerchoice = Math.floor(Math.random() * array.length);//random
+    compare(UserInput, Computerchoice);
+});
+scissorsbtn.addEventListener('click', function(){
+    UserInput = 2;//scissors
+    Computerchoice = Math.floor(Math.random() * array.length);//random
+    compare(UserInput, Computerchoice);
+});
+
