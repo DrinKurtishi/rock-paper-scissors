83975fd03265f489b04dbb0a010950f5a6574e32
DrinKurtishi <kurtishidrin@gmail.com>
2023-01-09 18:20:10 +0100

    letting user choose background color

--- rps.js
+++ rps.js
@@ -93,7 +93,7 @@ function DetermineOutcome(UserInput, Computerchoice){
            document.querySelector("#scissors").disabled = true;
            //determine winner
            if(UCount === 3){
-                document.getElementById("final-output").style.color = "lime";
+                document.getElementById("final-output").style.color = "#059c0f";
                 document.getElementById("final-output").innerHTML = "You defeated A.I.!"; 
            }
            else {
@@ -127,3 +127,33 @@ scissorsbtn.addEventListener('click', function(){//... when scissors
     Game();
 });
 
+//for background choosing
+function transition(){
+    document.body.style.transition="1s";
+}
+function purple(){
+    document.body.style.backgroundColor="#3a093a";
+    document.body.style.color = "whitesmoke";
+    transition();
+}
+function blue(){
+    document.body.style.backgroundColor = "#010150";
+    document.body.style.color = "whitesmoke";
+    transition();
+}
+function yellow(){
+    document.body.style.backgroundColor="#FFDB58";
+    document.body.style.color = "#121212";
+    transition();
+}
+function white(){
+    document.body.style.backgroundColor="white";
+    document.body.style.color = "#121212";
+    transition();
+}
+
+function black(){
+    document.body.style.backgroundColor="#121212";
+    document.body.style.color = "whitesmoke";
+    transition();
+}
