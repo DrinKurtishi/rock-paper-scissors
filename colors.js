6c16beab7ad476f64c3c2f3c23286d371cbc039c
DrinKurtishi <kurtishidrin@gmail.com>
2023-01-12 12:42:27 +0100

    Created Dark and light theme functions

--- colors.js
+++ colors.js
@@ -3,48 +3,43 @@
 function transition(){
     document.body.style.transition="1s";
 }
-function purple(){
-    document.body.style.backgroundColor="#3a093a";
+function DarkTheme(){
     document.body.style.color = "whitesmoke";
     document.getElementById("newRound").style.backgroundColor = "#202020";
     document.getElementById("newRound").style.color = "whitesmoke";
     document.getElementById("newGame").style.backgroundColor = "#202020";
     document.getElementById("newGame").style.color = "whitesmoke";
+}
+function LightTheme(){
+    document.body.style.color = "#121212";
+    document.getElementById("newRound").style.backgroundColor = "#cacccb";
+    document.getElementById("newRound").style.color = "#121212";
+    document.getElementById("newGame").style.backgroundColor = "#cacccb";
+    document.getElementById("newGame").style.color = "#121212";
+}
+function purple(){
+    document.body.style.backgroundColor="#3a093a";
+    DarkTheme();
     transition();
 }
 function blue(){
     document.body.style.backgroundColor = "#010150";
-    document.body.style.color = "whitesmoke";
-    document.getElementById("newRound").style.backgroundColor = "#202020";
-    document.getElementById("newRound").style.color = "whitesmoke";
-    document.getElementById("newGame").style.backgroundColor = "#202020";
-    document.getElementById("newGame").style.color = "whitesmoke";
+    DarkTheme();
     transition();
 }
 function yellow(){
     document.body.style.backgroundColor="#FFDB58";
-    document.body.style.color = "#121212";
-    document.getElementById("newRound").style.backgroundColor = "#cacccb";
-    document.getElementById("newRound").style.color = "#121212";
-    document.getElementById("newGame").style.backgroundColor = "#cacccb";
-    document.getElementById("newGame").style.color = "#121212";
+    LightTheme();
     transition();
 }
 function white(){
     document.body.style.backgroundColor="white";
-    document.body.style.color = "#121212";
-    document.getElementById("newRound").style.backgroundColor = "#cacccb";
-    document.getElementById("newRound").style.color = "#121212";
-    document.getElementById("newGame").style.backgroundColor = "#cacccb";
-    document.getElementById("newGame").style.color = "#121212";
+    LightTheme();
     transition();
 }
 function black(){
     document.body.style.backgroundColor="#121212";
     document.body.style.color = "whitesmoke";
-    document.getElementById("newRound").style.backgroundColor = "#202020";
-    document.getElementById("newRound").style.color = "whitesmoke";
-    document.getElementById("newGame").style.backgroundColor = "#202020";
-    document.getElementById("newGame").style.color = "whitesmoke";
+    DarkTheme();
     transition();
 }
