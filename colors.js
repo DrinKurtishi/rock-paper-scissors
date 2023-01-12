e62d6a25df57447d38fba7a2fc9eb53fc13df686
DrinKurtishi <kurtishidrin@gmail.com>
2023-01-12 01:51:53 +0100

    seperated bgcolor js code into seperate js file

---
+++ colors.js
@@ -0,0 +1,50 @@
+//for background choosing
+
+function transition(){
+    document.body.style.transition="1s";
+}
+function purple(){
+    document.body.style.backgroundColor="#3a093a";
+    document.body.style.color = "whitesmoke";
+    document.getElementById("newRound").style.backgroundColor = "#202020";
+    document.getElementById("newRound").style.color = "whitesmoke";
+    document.getElementById("newGame").style.backgroundColor = "#202020";
+    document.getElementById("newGame").style.color = "whitesmoke";
+    transition();
+}
+function blue(){
+    document.body.style.backgroundColor = "#010150";
+    document.body.style.color = "whitesmoke";
+    document.getElementById("newRound").style.backgroundColor = "#202020";
+    document.getElementById("newRound").style.color = "whitesmoke";
+    document.getElementById("newGame").style.backgroundColor = "#202020";
+    document.getElementById("newGame").style.color = "whitesmoke";
+    transition();
+}
+function yellow(){
+    document.body.style.backgroundColor="#FFDB58";
+    document.body.style.color = "#121212";
+    document.getElementById("newRound").style.backgroundColor = "#cacccb";
+    document.getElementById("newRound").style.color = "#121212";
+    document.getElementById("newGame").style.backgroundColor = "#cacccb";
+    document.getElementById("newGame").style.color = "#121212";
+    transition();
+}
+function white(){
+    document.body.style.backgroundColor="white";
+    document.body.style.color = "#121212";
+    document.getElementById("newRound").style.backgroundColor = "#cacccb";
+    document.getElementById("newRound").style.color = "#121212";
+    document.getElementById("newGame").style.backgroundColor = "#cacccb";
+    document.getElementById("newGame").style.color = "#121212";
+    transition();
+}
+function black(){
+    document.body.style.backgroundColor="#121212";
+    document.body.style.color = "whitesmoke";
+    document.getElementById("newRound").style.backgroundColor = "#202020";
+    document.getElementById("newRound").style.color = "whitesmoke";
+    document.getElementById("newGame").style.backgroundColor = "#202020";
+    document.getElementById("newGame").style.color = "whitesmoke";
+    transition();
+}
